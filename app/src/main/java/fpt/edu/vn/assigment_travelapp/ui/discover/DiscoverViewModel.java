482823252cb3980278865dc91727639f5c85d3a5
HuuAn03 <huuan2303@gmail.com>
2025-11-01 13:51:22 +0700

    add explore

---
+++ app/src/main/java/fpt/edu/vn/assigment_travelapp/ui/discover/DiscoverViewModel.java
@@ -0,0 +1,7 @@
+package fpt.edu.vn.assigment_travelapp.ui.discover;
+
+import androidx.lifecycle.ViewModel;
+
+public class DiscoverViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
