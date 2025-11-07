126d0e9a30972737670ae7329c8d893f5573e80a
Huy Minh <huyminh28062004@gmail.com>
2025-11-07 22:58:59 +0700

    [Update] App Theme and Add Toolbar

--- app/src/main/java/fpt/edu/vn/assigment_travelapp/MainActivity.java
+++ app/src/main/java/fpt/edu/vn/assigment_travelapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
 
         mAuth = FirebaseAuth.getInstance();
         createAdminUserIfNeeded();
